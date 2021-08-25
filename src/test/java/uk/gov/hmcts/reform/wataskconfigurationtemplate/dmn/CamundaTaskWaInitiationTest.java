290896a0187e52928ca4cc8c050f89cffbcee3f0
David Crespo <david.crespo@hmcts.net>
2021-08-25 16:00:00 +0100

    update DMNs table to get CEH non-req FT to pass (#4)
    * update DMNs table to get CEH non-req FT to pass
    
    * add unit coverage
    
    * add unit coverage

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaInitiationTest.java
@@ -6,9 +6,14 @@ import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.wataskconfigurationtemplate.DmnDecisionTableBaseUnitTest;
 
+import java.io.Serializable;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
@@ -22,30 +27,71 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         CURRENT_DMN_DECISION_TABLE = WA_TASK_INITIATION_WA_WACASETYPE;
     }
 
-    @Test
-    void given_multiple_event_ids_should_evaluate_dmn() {
+    @ParameterizedTest
+    @MethodSource("scenarioProvider")
+    void given_multiple_event_ids_should_evaluate_dmn(String eventId,
+                                                      String postEventState,
+                                                      String appealType,
+                                                      Map<String, ? extends Serializable> expectedDmnOutcome) {
         VariableMap inputVariables = new VariableMapImpl();
-        inputVariables.putValue("eventId", "CREATE");
-        inputVariables.putValue("postEventState", "TODO");
-        inputVariables.putValue("appealType", "anything");
+        inputVariables.putValue("eventId", eventId);
+        inputVariables.putValue("postEventState", postEventState);
+        inputVariables.putValue("appealType", appealType);
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
-        assertThat(dmnDecisionTableResult.getResultList(), is(singletonList(
-            Map.of(
-                "taskId", "processApplication",
-                "name", "Process Application",
-                "group", "TCW",
-                "workingDaysAllowed", 2
+        assertThat(dmnDecisionTableResult.getResultList(), is(singletonList(expectedDmnOutcome)));
+    }
+
+    public static Stream<Arguments> scenarioProvider() {
+        return Stream.of(
+            Arguments.of(
+                "CREATE", "TODO", "anything",
+                Map.of(
+                    "taskId", "processApplication",
+                    "name", "Process Application",
+                    "group", "TCW",
+                    "workingDaysAllowed", 2
+                )
+            ),
+            Arguments.of(
+                "submitCase", "caseUnderReview", "anything",
+                Map.of(
+                    "taskId", "reviewAppealSkeletonArgument",
+                    "name", "Review Appeal Skeleton Argument",
+                    "group", "TCW",
+                    "workingDaysAllowed", 2,
+                    "processCategories", "caseProgression"
+                )
+            ),
+            Arguments.of(
+                "submitCase", "caseUnderReview", "",
+                Map.of(
+                    "taskId", "reviewAppealSkeletonArgument",
+                    "name", "Review Appeal Skeleton Argument",
+                    "group", "TCW",
+                    "workingDaysAllowed", 2,
+                    "processCategories", "caseProgression"
+                )
+            ),
+            Arguments.of(
+                "submitCase", "caseUnderReview", null,
+                Map.of(
+                    "taskId", "reviewAppealSkeletonArgument",
+                    "name", "Review Appeal Skeleton Argument",
+                    "group", "TCW",
+                    "workingDaysAllowed", 2,
+                    "processCategories", "caseProgression"
+                )
             )
-        )));
+        );
     }
 
     @Test
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(1));
+        assertThat(logic.getRules().size(), is(2));
 
     }
 }
