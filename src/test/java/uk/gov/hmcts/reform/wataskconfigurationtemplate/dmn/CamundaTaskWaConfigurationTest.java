d3155db83321d72e611a30b9470662d95d976c12
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-02-17 11:41:36 +0000

    add additonal properties

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaConfigurationTest.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.Collections.emptyMap;
@@ -38,7 +39,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(9));
+        assertThat(logic.getRules().size(), is(13));
     }
 
     @SuppressWarnings("checkstyle:indentation")
@@ -145,6 +146,10 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
             .expectedLocationNameValue("Taylor House")
             .expectedCaseManagementCategoryValue("")
             .expectedDescription("")
+            .expectedAdditionalPropertiesKey1(null)
+            .expectedAdditionalPropertiesKey2(null)
+            .expectedAdditionalPropertiesKey3(null)
+            .expectedAdditionalPropertiesKey4(null)
             .build();
         String refusalOfEuLabel = "Refusal of a human rights claim";
         Scenario givenCaseDataIsPresentThenReturnNameAndValueScenario = Scenario.builder()
@@ -173,6 +178,10 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
             .expectedRoleCategory("LEGAL_OPERATIONS")
             .expectedDescription("[Decide an application]"
                                      + "(/case/WA/WaCaseType/${[CASE_REFERENCE]}/trigger/decideAnApplication)")
+            .expectedAdditionalPropertiesKey1("value1")
+            .expectedAdditionalPropertiesKey2("value2")
+            .expectedAdditionalPropertiesKey3("value3")
+            .expectedAdditionalPropertiesKey4("value4")
             .build();
 
         return Stream.of(
@@ -195,6 +204,10 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         String expectedWorkType;
         String expectedRoleCategory;
         String expectedDescription;
+        String expectedAdditionalPropertiesKey1;
+        String expectedAdditionalPropertiesKey2;
+        String expectedAdditionalPropertiesKey3;
+        String expectedAdditionalPropertiesKey4;
     }
 
     private List<Map<String, String>> getExpectedValues(Scenario scenario) {
@@ -213,6 +226,15 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
             getExpectedValue(rules, "roleCategory", scenario.getExpectedRoleCategory());
         }
         getExpectedValue(rules, "description", scenario.getExpectedDescription());
+
+        Optional.ofNullable(scenario.getExpectedAdditionalPropertiesKey1())
+            .ifPresent(key -> getExpectedValue(rules, "additionalProperties_key1", key));
+        Optional.ofNullable(scenario.getExpectedAdditionalPropertiesKey2())
+            .ifPresent(key -> getExpectedValue(rules, "additionalProperties_key2", key));
+        Optional.ofNullable(scenario.getExpectedAdditionalPropertiesKey3())
+            .ifPresent(key -> getExpectedValue(rules, "additionalProperties_key3", key));
+        Optional.ofNullable(scenario.getExpectedAdditionalPropertiesKey4())
+            .ifPresent(key -> getExpectedValue(rules, "additionalProperties_key4", key));
         return rules;
     }
 
