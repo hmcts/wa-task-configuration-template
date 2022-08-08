8283e20e64f5c14f773228c3259e9c885cba138e
Liibaan Xassan <burale2000@hotmail.com>
2022-08-08 14:30:37 +0100

    RWA-1069 Re-visit the CEH FTs which are marked as @Ignore (#34)

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaConfigurationTest.java
@@ -43,7 +43,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(22));
+        assertThat(logic.getRules().size(), is(26));
     }
 
     @SuppressWarnings("checkstyle:indentation")
