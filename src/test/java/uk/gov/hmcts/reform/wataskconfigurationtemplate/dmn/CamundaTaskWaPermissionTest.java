2d50836a5cfda066d24007a7249eb1e72053abac
oguz akin <akinserdaroguzhan@gmail.com>
2022-08-12 17:14:53 +0100

    fix failing test

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskWaPermissionTest.java
@@ -361,7 +361,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
             Map.of(
                 "name", "task-supervisor",
-                "value", "Read,Refer,Manage,Cancel",
+                "value", "Read,Manage,Cancel,Assign,Unassign,Unassignassign,Complete",
                 "autoAssignable", false,
                 "caseAccessCategory", "categoryA"
             ),
